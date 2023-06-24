db04edd2633cd4c0f26ad4891a2b07f075c8f707
Yogesh More <moreyog@gmail.com>
2023-06-24 15:29:53 +0530

    RateLimit with Filter based working

--- config/GatewayConfig.java
+++ config/GatewayConfig.java
@@ -7,22 +7,22 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class GatewayConfig {
 
     @Autowired
     RateLimitFilter rateLimitFilter;
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        System.out.println(rateLimitFilter);
-        return builder.routes()
-                .route("swiggy-app", r -> r.path("/swiggy/**")
-                        .filters(f -> f.filter(rateLimitFilter.apply(new RateLimitFilter.Config())).addRequestHeader("X-Gold-Plan", "true"))
-                        .uri("lb://BACKEND1-SERVICE"))
-                .route("payment-app", r -> r.path("/payment/**")
-                        .filters(f -> f.filter(rateLimitFilter.apply(new RateLimitFilter.Config())).addRequestHeader("X-Silver-Plan", "true"))
-                        .uri("lb://BACKEND2-SERVICE"))
-                .build();
-    }
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        System.out.println(rateLimitFilter);
+//        return builder.routes()
+//                .route("swiggy-app", r -> r.path("/swiggy/**")
+//                        .filters(f -> f.filter(rateLimitFilter.apply(new RateLimitFilter.Config())).addRequestHeader("X-Gold-Plan", "true"))
+//                        .uri("lb://BACKEND1-SERVICE"))
+//                .route("payment-app", r -> r.path("/payment/**")
+//                        .filters(f -> f.filter(rateLimitFilter.apply(new RateLimitFilter.Config())).addRequestHeader("X-Silver-Plan", "true"))
+//                        .uri("lb://BACKEND2-SERVICE"))
+//                .build();
+//    }
 }
